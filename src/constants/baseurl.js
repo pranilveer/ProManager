a479aed20f4cc19adeee28be4e3fac2990a3aa2c
Pranil Dilip Veer <veerpranil@gmail.com>
2024-03-03 14:18:34 +0530

    url change

--- src/constants/baseurl.js
+++ src/constants/baseurl.js
@@ -1,4 +1,2 @@
 export const BACKEND_URL = "https://promanage-backend-1.onrender.com/api";
-// export const BACKEND_URL = "http://localhost:4000/api";
-export const FRONTEND_URL = "http://promanagecuvette.netlify.app";
-// export const FRONTEND_URL = "http://localhost:3000";
+export const FRONTEND_URL = "https://promanagecuvette.netlify.app";
